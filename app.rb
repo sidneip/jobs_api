f0f5c921d3e0caae29a079973fb5cf1cfc2fd4c5
Sidnei Pacheco <sidnei.junior@hubfintech.com.br>
2017-02-22 19:19:17 -0300

    job params

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require "sinatra/base"
 require "sinatra/activerecord"
 require "json"
+require 'byebug'
 
 # MODELS
 require "./models/category"
@@ -18,13 +19,31 @@ class JobsApplication < Sinatra::Base
     content_type :json
   end
 
-  get "/jobs" do
-    @posts = Job.order(updated_at: :desc).paginate(:page => params[:page], :per_page => per_page)
+  get '/jobs' do
+    @posts = Job
+             .order(updated_at: :desc)
+             .paginate(page: params[:page], per_page: per_page)
     @posts.to_json
+  end
+
+  post '/jobs' do
+    job_service = JobService.new(job_params).create
+    job_service.to_json
   end
 
   def per_page
     params[:per_page] || 10
-   end
+  end
+
+  def job_params
+    params = JSON.parse(request.body.read)
+    params.slice(
+      'category_id',
+      'partner_id',
+      'expired_at',
+      'title',
+      'category_external_code'
+    )
+  end
 
 end
