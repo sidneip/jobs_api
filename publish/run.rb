ce9230101964842ae08abcb4117b4846dc3970fa
Sidnei Pacheco <sidnei.junior@hubfintech.com.br>
2017-02-23 15:13:32 -0300

    relative filepath

--- publish/run.rb
+++ publish/run.rb
@@ -1,5 +1,5 @@
 require_relative 'sidekiq_script.rb'
 require_relative 'workers/import_jobs_worker.rb'
-path = '/app/publish/jobs.txt'
+path = File.expand_path("../jobs.txt", __FILE__) #'/app/publish/jobs.txt'
 puts "###### START IMPORT FILE ######"
 ImportJobsWorker.perform_async(path)
